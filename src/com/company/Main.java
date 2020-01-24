bb5120916c88efb94dfb05d5c556d4f7cfd7bef0
serian <sseriann@mail.ru>
2020-01-24 18:41:03 +0400

    3_7

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,12 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> colors = new ArrayList<>();
-        colors.add("Green");
-        colors.add("Yellow");
-        colors.add("Red");
-        colors.add("Black");
-        colors.add("Orange");
-        for (int i = 0; i < colors.size(); i++) {
-            System.out.println(colors.get(i));
-        }
-        System.out.println();
-        colors.remove(2);
-        for (int i = 0; i < colors.size(); i++) {
-            System.out.println(colors.get(i));
-        }
-
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(50);
+        numbers.add(10);
+        numbers.add(30);
+        numbers.add(20);
+        numbers.add(40);
+        System.out.println(numbers.indexOf(20));
     }
 }
