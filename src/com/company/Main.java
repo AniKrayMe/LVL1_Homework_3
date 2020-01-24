73129a4726d290665cc84e136bf1cd39b3f2e380
serian <sseriann@mail.ru>
2020-01-24 18:42:19 +0400

    3_11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,32 @@
 package com.company;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Integer> numbers = new ArrayList<>();
-        numbers.add(50);
-        numbers.add(10);
-        numbers.add(30);
-        numbers.add(20);
-        numbers.add(40);
-        for (int i = 0; i <numbers.size(); i++) {
-            System.out.println(numbers.get(i));
+        ArrayList<Integer> numbers1 = new ArrayList<>();
+        numbers1.add(50);
+        numbers1.add(10);
+        numbers1.add(30);
+        numbers1.add(20);
+        numbers1.add(40);
+        for (int i = 0; i < numbers1.size(); i++) {
+            System.out.print(numbers1.get(i) + ", ");
 
         }
         System.out.println();
-        Collections.reverse(numbers);
-        for (int i = 0; i <numbers.size(); i++) {
-            System.out.println(numbers.get(i));
+        ArrayList<Integer> numbers2 = new ArrayList<>();
+        numbers2.add(60);
+        numbers2.add(70);
+        numbers2.add(80);
+        numbers2.add(90);
+        numbers2.add(100);
+        for (int i = 0; i < numbers1.size(); i++) {
+            System.out.print(numbers1.get(i) + ", ");
+
+
         }
+        System.out.println(numbers1.equals(numbers2));
     }
 }
