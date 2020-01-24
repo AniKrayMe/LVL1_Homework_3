1eb908bf0ba9ec7d981c92e0b1836b9f224e457d
serian <sseriann@mail.ru>
2020-01-24 18:39:19 +0400

    3_2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,8 @@
 package com.company;
+
 import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,9 +12,15 @@ public class Main {
         colors.add("Red");
         colors.add("Black");
         colors.add("Orange");
-        for (int i = 0; i <colors.size(); i++) {
+        for (int i = 0; i < colors.size(); i++) {
             System.out.println(colors.get(i));
         }
+        Iterator<String> colorsIterator = colors.iterator();
+        for (int i = 0; colorsIterator.hasNext(); i++) {
+            String color = colorsIterator.next();
+            System.out.println(color);
 
+
+        }
     }
 }
